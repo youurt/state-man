254ebbf2d2fbfec5fb5c433554b26f7c46b6b2cf
Tigu Ugur <u.tigu@softfair.de>
2025-01-30 16:27:11 +0100

    styling

--- src/components/todo-list/todo-list.component.ts
+++ src/components/todo-list/todo-list.component.ts
@@ -1,10 +1,9 @@
-import { NgFor } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 import { Todo } from 'src/models/todo.model';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
-  imports: [NgFor, TodoItemComponent],
+  imports: [TodoItemComponent],
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss'],
